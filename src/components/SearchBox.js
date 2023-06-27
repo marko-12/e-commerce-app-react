8c6a92ea29c5ded4ec7dfeb9ac3181e89b2bc7bd
marko-12 <marko96.mn@gmail.com>
2023-06-27 14:26:29 +0200

    Dynamic Search Completed, (Autocomplete not 100%)

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,33 +4,82 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import { useNavigate } from "react-router-dom";
-import { wait } from "@testing-library/user-event/dist/utils";
+import { useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import axios from "axios";
+import { blue, red } from "@mui/material/colors";
 
 export default function SearchBox() {
   const navigate = useNavigate();
-  const [query, setQuery] = useState("");
-  const submitHandler = (e) => {
+  //const [query, setQuery] = useState("");
+  const searchHandler = (e) => {
     e.preventDefault();
-    setQuery(e.target.value);
+    const query = e.target.value;
     navigate(query ? `/search/?query=${query}` : "/search");
   };
+  const search = () => {
+    searchHandler();
+  };
+  const [myOptions, setMyOptions] = useState([]);
 
+  function getData() {
+    // fetch data
+    fetch("api/products")
+      .then((response) => {
+        return response.json();
+      })
+      .then((res) => {
+        for (var i = 0; i < res.length; i++) {
+          if (!myOptions.some((obj) => obj.label == res[i].name)) {
+            // create an object with a label
+            let object = {
+              label: res[i].name,
+              usersName: res[i].name,
+            };
+            myOptions.push(object);
+          }
+        }
+        setMyOptions(myOptions);
+      });
+  }
+  useEffect(() => getData(), [myOptions]);
+  //useEffect(() => search(), [query]);
   return (
-    <Form className="d-flex me-auto" onSubmit={submitHandler}>
-      <InputGroup>
-        <FormControl
-          type="text"
-          name="q"
-          id="q"
-          onChange={submitHandler}
-          placeholder="search products..."
-          aria-label="Search Products"
-          aria-describedby="button-search"
-        ></FormControl>
-        <Button variant="outline-primary" type="submit" id="button-search">
-          <i className="fas fa-search"></i>
-        </Button>
-      </InputGroup>
-    </Form>
+    // <Form className="d-flex me-auto">
+    //   <InputGroup>
+    //     <FormControl
+    //       type="text"
+    //       name="q"
+    //       id="q"
+    //       onChange={searchHandler}
+    //       placeholder="search products..."
+    //       aria-label="Search Products"
+    //       aria-describedby="button-search"
+    //     ></FormControl>
+    //     <Button variant="outline-primary" type="submit" id="button-search">
+    //       <i className="fas fa-search"></i>
+    //     </Button>
+    //   </InputGroup>
+    // </Form>
+    <div>
+      <Autocomplete
+        style={{ width: 500, backgroundColor: "whitesmoke" }}
+        autoComplete
+        autoHighlight
+        freeSolo
+        clearOnEscape
+        options={myOptions}
+        renderInput={(data) => (
+          <TextField
+            {...data}
+            variant="outlined"
+            label="Search Products"
+            onChange={searchHandler}
+            //={searchHandler}
+          />
+        )}
+      />
+    </div>
   );
 }
